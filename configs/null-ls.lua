ce5c7e19d0a6b423d185d622ac0c53bf09343e7d
ngthorg <ngthorg@gmail.com>
2023-07-31 13:21:09 +0700

    feat: add my configs

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -10,7 +10,19 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "yaml", "json", "javascript", "javascriptreact", "typescript", "typescriptreact" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with {
+    filetypes = {
+      "html",
+      "markdown",
+      "css",
+      "yaml",
+      "json",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+  }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
